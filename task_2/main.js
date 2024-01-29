3a45fc817f1dbc6e0b3b49c98294657cd808ebc5
miraxxel <forremains@yandex.ru>
2024-01-29 10:46:26 +0300

    Написали программу с вложенными условиями

--- task_2/main.js
+++ task_2/main.js
@@ -1,6 +1,21 @@
-const age = 16;
+/*const age = 16;
 if (age >= 18) {
     console.log("Вы совершеннолетний!");
 } else {
     console.log("Вы не щсовершеннолетний!");
+} */
+
+let statusPar = true;
+console.log("Сегодня важные пар?");
+
+if (statusPar === true) {
+    console.log("Сколько сегодня пар?");
+    let countPar = 2;
+    if (countPar < 4) {
+        console.log("Сиди дома");
+    } else {
+        console.log("Беги на учебу");
+    }
+} else {
+    console.log("Сиди дома");
 }
