c28bbfc6241990dd4698feef9229cf403a43856c
miraxxel <forremains@yandex.ru>
2024-02-05 13:54:17 +0300

    Работа с объектами

--- task_3/main.js
+++ task_3/main.js
@@ -1,75 +1,56 @@
-/*const students =  ["Андрей", "Иван", "Стас"];
-const student = students[1];
-console.log(student);*/
+// объекты пишутся только в фигурных скобках
+/*let person = {
+    name: "Tom",
+    age:  18,
+    hobbies: ["Кушать", "Спать", "Просмотр кино"],
+    contacts: {
+        phone: [88005553255, 889055553255],
+        email: "tom@example.com",
+        social: "www.github.com"
+    }
+}
 
-// js такой язык в котором лучше всё делать отдельно, 
-// а в console.log(); выводить только уже готовый параметр
-// (например с примером выше, лучше не делать сразу console.log(students[1]);, а исп. отдел. переменную)
-
-
-// при изменении элемента в массиве, менять можно на любой элемент не зависимо от типа остальных элементов
-/*let students =  ["Андрей", "Иван", "Стас"]; 
-students[2] = 5
-console.log(students);*/
-
-
-// если заранее нет нужного индекса в массиве, его можно создать
-// (не добавляя элементы в массив пока их в нём не станет 50)
-// (в консоли выдаст только заполненые индексы, (укажет что пусто ещё 47))
-/*let students =  ["Андрей", "Иван", "Стас"]; 
-students[50] = "Индекс 50";
-console.log(students);*/
+console.log(person["name"], person["age"]) // обращение к атрибуту, сначала к объекту, затем в [""] имя атрибута
+// или
+console.log(`Имя - ${person["name"]}. Возраст - ${person["age"]}`); 
+console.log(person["hobbies"][2]) // переходим к глобальному объекту, а после выбираем конкретный элемент
+console.log(person["contacts"]["phone"][1]);*/
 
 
-// ctrl+/ - закомментировать
+// массив сам индексирует элементы
+// объект (более глобальный, чем массив, индекс задаем сами, пишем в {}) содержит атрибуты которые мы сами придумываем
+// массив мы исп. когда не знаем как индексировать наши значения (например, какие пары сегодня)
+// то к чему мы можем приписать общее определение, мы используем атрибуты 
+//(например, как выглядит студент, что у него на голове, что на руках, вверх, них и т.д)
 
-// многомерные массивы - массив содержащий в себе вложенные массивы
-/*let college = [ ["Андрей", "Иван", "Стас"], [205, 111, 304] ];
+
+/*let college = [
+    {name: "Tom", age:  18, group: "ИС-2-1"},
+    {name: "Bob", age:  18, group: "ИС-2-2"},
+    {name: "Stan", age:  23, group: "ИС-2-1"},
+    {name: "Harry", age:  17, group: "ИС-2-2"},
+];
+
+college[0]["age"] = 24;
 console.log(college);*/
 
-// в многомерном массиве, помимо элементов, сами массивы имеют свой индекс
-// как вывести элемент из многомерного массива
-// сначала выбираем индекс нужного массива
-// а затем индекс нужного элемента
-// если массивов больше, то это становится похоже на структуру проводника
-/*let college = [ ["Андрей", "Иван", "Стас"], [205, 111, 304] ];
-college[1][1] = 112;
-let kab = college[1][1];
-console.log(kab);*/
+//console.log(college[2]["name"]); // для атрибута, при выводемы указываем его имя
+// если атрибуты объектов одинаковые их можно хранить в массиве
 
+let college = [
+    {name: "Tom", age:  18, group: "ИС-2-1"},
+    {name: "Bob", age:  18, group: "ИС-2-2"},
+    {name: "Stan", age:  23, group: "ИС-2-1"},
+    {name: "Harry", age:  17, group: "ИС-2-2"},
+];
 
-// атрибут length (определение длины) создается автоматически 
-// let num = [1, 2, 3, 4];
+let count = 2;
 
-// const countLength = 5;
+let studentAge = college[count]["age"];
+let studentName = college[count]["name"];
 
-// if (num.length === countLength) {
-//     console.log("Ok");
-// } else if (num.length < countLength) {
-//     let result = countLength - num.length;
-//     console.log(`Длина вашего массива: ${num.length}. Длину массива нужно увеличить до ${countLenght}. Прибавьте к массиву `);
-// }
-
-
-// косые ковычки - передают динамический параметр
-
-/*let number = [ 1, 2, 3, 4, 5, 6, 7 ];
-
-const coutLenght = 5;
-
-if ( number.length === coutLenght ) {
-    console.log("ok");
-} 
-else if ( number.length < coutLenght ) {
-    let result = coutLenght - number.length;
-    console.log(`Длина вашего массива: ${number.length}. Длину массива нужно увеличить до ${coutLenght}. Прибавьте к массиву значение ${result}.`);
-} 
-else if ( number.length > coutLenght ) {
-    result = number.length - coutLenght;
-    console.log(`Длина вашего массива: ${number.length}. Длину массива нужно уменьшить до ${coutLenght}. Удалите из массива количество элементов: ${result}.`);
-}*/
-
-
-
-let number = [ [1, 2, 3], [4, 5, 6, 7], ["ИС-2-2", "ИС-2-1"] ];
-const arrayLen = number.length;
+if (studentAge >= 18 ) {
+    console.log(`Студент ${studentName} совершеннолетрий, ему ${studentAge}`);
+} else if (studentAge < 18) {
+    console.log(`Студент ${studentName} не совершеннолетрий, ему ${studentAge}`);
+}
